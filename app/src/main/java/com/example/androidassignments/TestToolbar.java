db94a0af397d9340c1abfcf10f1c2c96d5a08196
andrew <a.liutiev@gmail.com>
2022-10-23 23:54:17 -0400

    Finished Assignment2Part4

---
+++ app/src/main/java/com/example/androidassignments/TestToolbar.java
@@ -0,0 +1,101 @@
+package com.example.androidassignments;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.androidassignments.databinding.ActivityTestToolbarBinding;
+
+public class TestToolbar extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityTestToolbarBinding binding;
+
+
+    String snackBarMessage = "You have selected item number 1";
+    EditText newTextValue;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton floatingButton = findViewById(R.id.floatingButton);
+        floatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "You selected item 1", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu m){
+        getMenuInflater().inflate(R.menu.toolbar_menu, m);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemID = item.getItemId();
+
+        switch(itemID){
+            case R.id.option1:
+                Snackbar.make(getWindow().getDecorView(), snackBarMessage, Snackbar.LENGTH_LONG).show();
+                Log.d("Toolbar", "Option 1 was selected");
+                break;
+            case R.id.option2:
+                AlertDialog.Builder builder = new AlertDialog.Builder(TestToolbar.this);
+                builder.setTitle(R.string.goBack);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        finish();
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                    }
+                });
+                builder.show();
+                Log.d("Toolbar", "Option 2 was selected");
+                break;
+            case R.id.option3:
+
+                Log.d("Toolbar", "Option 3 selected");
+                break;
+            case R.id.option4:
+                Toast.makeText(this, "Version 1.0, by Thenura Jayasinghe", Toast.LENGTH_LONG).show();
+                Log.d("Toolbar", "Option 4 selected");
+                break;
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_test_toolbar);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
